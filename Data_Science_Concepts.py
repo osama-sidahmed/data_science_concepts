ae12dc0bf47f63555ef388a3c5356985ac3fa4d8
Osama Sidahmed <omassoft123@gmail.com>
2021-11-22 08:19:11 -0500

    Update .py Overfitting plot

--- Data_Science_Concepts.py
+++ Data_Science_Concepts.py
@@ -14,33 +14,29 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import joblib
 
-# Some information
-'''
-Text Text Text Text Text Text Text 
-Text Text Text Text Text Text Text
-Text Text Text Text Text Text Text 
+# This function prints markdown txt
+def msg(message, font_size):
 
-'''
+    msg_str = f"""
+    <style>
+    p.a {{
+    font: bold {font_size}px Courier;
+    }}
+    </style>
+    <p class="a">{message}</p>
+    """
 
-# What to write here
-
-# Load data to be used in the streamlit app.
-# @st.cache
-# def load_data(path, num_rows):
-#     df = pd.read_csv(path, nrows=num_rows, index_col=0)
-#     df = df.rename(columns={'Start Station Latitude': 'lat',
-#         'Start Station Longitude': 'lon'})
-#     df['Start Time'] = pd.to_datetime(df['Start Time'])
-#     df['Stop Time'] = pd.to_datetime(df['Stop Time'])
-#     return df
-
-# Load cached data with a function defined.
-# df = load_data('data/NYC_bikes.csv', 10000)
+    st.markdown(msg_str, unsafe_allow_html=True)
 
 ##############################
+##############################
 
-# Set the random seed for reproduceable output.
-np.random.seed(123)
+# Some information
+message = 'Hi Hi'
+msg(message, 15)
+
+##############################
+##############################
 
 # Create synthetic data to to use for illustration
 
@@ -50,26 +46,44 @@ x = np.linspace(-1, 1, 100)*2
 # Creates the target variable
 y = 5*x**3 + 2.5*x**2 + 11 + np.random.randn(100)*5.0 
 
-
-# This the function above
+# This function above
 def f(t):
     return 5*t**3 + 2.5*t**2 + 11
 
-# Plot
-fig = plt.figure()
-plt.scatter(x, y, c="cornflowerblue", label = "data") # Plot the points
-plt.plot(x, f(x), 'b--', label="function") # Plot the driving function (no error)
-plt.xlabel("x (independent variable)") 
-plt.ylabel("y (target)")
-plt.title("Plotted Synthetic Data")
-plt.legend()
-st.write(fig)
+##################################
+
+# This function prints markdown txt
+def msg(message, font_size):
+
+    msg_str = f"""
+    <style>
+    p.a {{
+    font: bold {font_size}px Courier;
+    }}
+    </style>
+    <p class="a">{message}</p>
+    """
+
+    st.markdown(msg_str, unsafe_allow_html=True)
 
 ##################################
 
-##################################
+# This function displays Overfitting plots
+def plot_overfitting(x, y, label_1, fx, label_2, y_pred, xlabel, ylabel, title):
+    fig1 = plt.figure()
+    plt.scatter(x, y, label=label_1, c="cornflowerblue") # Plot the data
+    plt.plot(x, fx, 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
+    plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
+    plt.legend()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    st.write(fig1)
 
 ##################################
+##################################
+
+# This function loads all saved models
 @st.cache(allow_output_mutation=True)
 def load_model(model_type):
     if model_type == 1:
@@ -89,154 +103,82 @@ def load_model(model_type):
 ##################################
 
 ##################################
-# @st.cache
-# def plot_model(x, y, model_type):
-
-#     # if model_type == 1:
-#     #     x = x.reshape(-1,1)
-
-#     # # Make predictions
-#     # y_pred = model.predict(x)
-
-#     # Plot and fit curve
-
-#     fig = plt.figure()
-#     plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-#     # plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-#     # plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-#     plt.legend()
-#     plt.xlabel('x')
-#     plt.ylabel('y')
-#     plt.title('Curve fitting')
-
-#     # Show
-#     return fig
-##################################
-
-##################################
 
 ## Create a main sidebar
 
-menu_list = ('Overfitting', 'Regularization', 'Neural network', 'Difference between svm and logistic regression',
- 'Accuracy Vs Recall/ etc and the confusion matrix', 'ROC and AUC', 'Profitability curves', 'Lift')
+menu_list = ('Select', 'Overfitting', 'SVM Vs Logistic regression', 'Regularization',
+ 'Accuracy Vs Recall/ etc and the confusion matrix', 
+ 'ROC and AUC', 'Neural networks', 'Profitability curves', 'Lift')
 
+st.sidebar.title('Topics')  
+topics_drop_down = st.sidebar.selectbox('Choose a topic', menu_list, index = 0)
 
-match menu_list:
-        case 'Overfitting':
+if topics_drop_down == 'Overfitting':
 
-            st.sidebar.title('Topics')  
-            st.sidebar.selectbox('Choose a topic', menu_list)
+    # # Plot
+    # fig = plt.figure()
+    # plt.scatter(x, y, c="cornflowerblue", label = "data") # Plot the points
+    # plt.plot(x, f(x), 'b--', label="function") # Plot the driving function (no error)
+    # plt.xlabel("x (independent variable)") 
+    # plt.ylabel("y (target)")
+    # plt.title("Plotted Synthetic Data")
+    # plt.legend()
+    # st.write(fig)
 
-            ## Create a sidebar
-            st.sidebar.title('Filters')
-            st.sidebar.markdown('''Choose model type''')
+    ## Create a sidebar
+    st.sidebar.title('Filters')
+    st.sidebar.markdown('''Choose model type''')
 
-            # A checkbox in sidebar to toggle roundtrip.
-            model_type = st.sidebar.radio('pick a model type', \
-                ('Underfit', 'Fit (about right)', 'Overfit_50K', 'Overfit_75K', 'Overfit_100K', 'Overfit_125K'))
-            # round trip condition based on checkbox
-            if model_type == 'Underfit':
-                model = load_model(1)
-                # fig1 = plot_model(x, y, 1)
-                x_reshaped = x.reshape(-1,1)
+    # A checkbox in sidebar to toggle roundtrip.
+    topics_Overfitting = st.sidebar.radio('pick a model type', \
+        ('Underfit', 'Fit (about right)', 'Overfit_50K', 'Overfit_75K', 'Overfit_100K', 'Overfit_125K'))
+    # round trip condition based on checkbox
 
-                # # Make predictions
-                y_pred = model.predict(x_reshaped)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+    # Load and predict all models to speed togglling between options
+    x_reshaped = x.reshape(-1,1)
+    model_1 = load_model(1)
+    y_pred_1 = model_1.predict(x_reshaped)
+    model_2 = load_model(2)
+    y_pred_2 = model_2.predict(x_reshaped)
+    model_3 = load_model(3)
+    y_pred_3 = model_3.predict(x_reshaped)
+    model_4 = load_model(4)
+    y_pred_4 = model_4.predict(x_reshaped)
+    model_5 = load_model(5)
+    y_pred_5 = model_5.predict(x_reshaped)
+    model_6 = load_model(6)
+    y_pred_6 = model_6.predict(x_reshaped)
 
-            elif model_type == 'Fit (about right)':
-                model = load_model(2)
-                # # Make predictions
-                y_pred = model.predict(x)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+    if topics_Overfitting == 'Underfit':
 
-            elif model_type == 'Overfit_50K':
-                model = load_model(3)
-                y_pred = model.predict(x)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+        plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_1, 'X', 'Y', 'Underfit Model')
 
-            elif model_type == 'Overfit_75K':
-                model = load_model(4)
-                y_pred = model.predict(x)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+    elif topics_Overfitting == 'Fit (about right)':
 
-            elif model_type == 'Overfit_100K':
-                model = load_model(5)
-                y_pred = model.predict(x)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+        plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_2, 'X', 'Y', 'Fit (about right) Model')
 
-            elif model_type == 'Overfit_125K':
-                model = load_model(6)
-                y_pred = model.predict(x)
-                fig1 = plt.figure()
-                plt.scatter(x, y, label='True labels', c="cornflowerblue") # Plot the data
-                plt.plot(x, f(x), 'b--', label="Function", alpha=0.5) # Plot the driving function (no error)
-                plt.plot(x, y_pred, linewidth=2, color='r', linestyle='--', label='Model') # Plot the predictions
-                plt.legend()
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.title('Curve fitting')
-                st.write(fig1)
+    elif topics_Overfitting == 'Overfit_50K':
 
-# # A checkbox in sidebar to toggle day of the week slider.
-# use_day = st.sidebar.checkbox('Filter By Day')
-# # A slider in sidebar to change day of the week.
-# if use_day == True:
-#     day_filter = st.sidebar.slider('Day of the week', 1,7,1)
-#     df = df[df['Start Time'].dt.weekday == (day_filter -1)]
+        plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_3, 'X', 'Y', 'Overfit_50K epochs Model')
 
-# # A checkbox in sidebar to toggle hour slider.
-# use_hour = st.sidebar.checkbox('Set hour')
-# if use_hour == True:
-#     # Have the sidebar determine an hour of the day.
-#     hour = st.sidebar.slider('Hour', 0, 24, 12)
-#     df = df[df['Start Time'].dt.hour == hour]
+    elif topics_Overfitting == 'Overfit_75K':
 
+        plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_4, 'X', 'Y', 'Overfit_75K epochs Model')
 
-# Create a plot to show the distribution of birth years.
-# fig = plt.figure()
-# plt.hist(df['Birth Year'], color="cornflowerblue", bins = 50)
-# plt.title("Distribution of Birth Year of Customers")
-# plt.xlabel("Year")
-# plt.ylabel("Frequency")
-# st.write(fig)
+    elif topics_Overfitting == 'Overfit_100K':
+        
+        plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_5, 'X', 'Y', 'Overfit_100K epochs Model')
+
+    elif topics_Overfitting == 'Overfit_125K':
+
+         plot_overfitting(x, y, 'Data', f(x),'Function', 
+                        y_pred_6, 'X', 'Y', 'Overfit_125K epochs Model')
+
+else:
+    if topics_drop_down != menu_list[0]:
+        msg(topics_drop_down +  ' is coming soon!', 30)
